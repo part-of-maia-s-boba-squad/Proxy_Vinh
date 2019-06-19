5bd2626e4c52c5fde8ddd472c069ef0abed2da01
Vinh Tran <vincen7.tran@gmail.com>
2019-06-18 17:24:33 -0700

    Proxy to NGINX

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:8-alpine
+RUN mkdir -p /app
+WORKDIR /app
+COPY . /app
+RUN npm install
+EXPOSE 3000
+CMD [ "npm", "start"]
--- server.js
+++ server.js
@@ -8,9 +8,13 @@ const port = process.env.PORT || 3000;
 const publicDir = path.join(__dirname, './public');
 
 app.use(express.static(publicDir));
-app.use('/API/restaurant/photo', proxy('http://localhost:3002/API/restaurant/photo'));
-app.use('/photo', proxy('http://localhost:3002/photo'));
-app.use('/flag', proxy('http://localhost:3002/flag'));
+app.use('/API/restaurant/photo', proxy('http://ec2-13-52-178-96.us-west-1.compute.amazonaws.com/API/restaurant/photo'));
+app.use('/photo', proxy('http://ec2-13-52-178-96.us-west-1.compute.amazonaws.com/photo'));
+app.use('/flag', proxy('http://ec2-13-52-178-96.us-west-1.compute.amazonaws.com/flag'));
+
+app.get('/loaderio-ed289c50a7ca37d416718c84afbbd840/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, './loaderio-ed289c50a7ca37d416718c84afbbd840.txt'));
+});
 
 app.get('/:id', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '/public') + '/index.html');
